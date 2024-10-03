f45d886e4660e38da968809e8aebc2ef2971e08d
LucasCoutinho200563162 <134244960+LucasCoutinho200563162@users.noreply.github.com>
2024-10-03 11:59:10 -0400

    Add files via upload

--- Assignment_2/funcs.js
+++ Assignment_2/funcs.js
@@ -1,7 +1,11 @@
+// Function has default values, does not return anything, just concatenate 3 sentences and print on the console
 export const printingText = (firstSentence =  "Hello", secondSentence =  "World!", thirdSentence =  ":D") => {
     console.log(firstSentence.concat(" ", secondSentence, " ", thirdSentence))
 }
 
+
+// Function that receives values for the speed value and origin unit (ms or kmh) then return the 
+// converted speed. If origin unit is ms then convert to kmh, if origin unit is kmh then convert to ms
 export const speedConverter = (speedValue = 60, originUnit = "kmh") => {
     if ( originUnit ==  "kmh") {
         return speedValue / 3.6
@@ -13,11 +17,17 @@ export const speedConverter = (speedValue = 60, originUnit = "kmh") => {
     
 }
 
+
+
+// Function to double the value given as input
 export const double = (number = 2) => {
     return number * 2
 }
 
+
+// Function to check if the number given is a prime number
 const isPrime = (number = 2) => {
+    // Checking if the number is divisible from 2 to its sqrt
     for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i == 0) {
             return false
--- Assignment_2/main.js
+++ Assignment_2/main.js
@@ -1,9 +1,17 @@
-// Import using CJS
+// Import using ES
 import {printingText, speedConverter, double, primeCheck} from './funcs.js' 
 
 
-
+// Testing priting function with and without input
 printingText("This is", "our second", "assignment.")
+printingText()
+
+
+// Testing speed covnerter, from ms to kmh
 console.log(speedConverter(55.7, "ms"))
+
+// Testing function to double value
 console.log(double(100.06))
+
+// Testing function to check if number is prime
 console.log(primeCheck(101))
