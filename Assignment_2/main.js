6635add84740b4a065fd303fcb41015aec689c5b
minameaning <saipetch9225@gmail.com>
2024-10-03 22:00:55 -0400

    add human-readable text in each log

--- Assignment_2/main.js
+++ Assignment_2/main.js
@@ -1,17 +1,28 @@
 // Import using ES
-import {printingText, speedConverter, double, primeCheck} from './funcs.js' 
-
+import { printingText, speedConverter, multiplyBy, primeCheck } from "./funcs.js";
 
 // Testing priting function with and without input
-printingText("This is", "our second", "assignment.")
-printingText()
+printingText("This is", "our second", "assignment.");
+printingText();
 
+const meterPerSecUnit = "ms";
+const kilometerPerHourUnit = "kmh";
 
 // Testing speed covnerter, from ms to kmh
-console.log(speedConverter(55.7, "ms"))
+const firstSpeedValue = 55.7;
+console.log(
+  "from",
+  `${firstSpeedValue} ${meterPerSecUnit}`,
+  "to",
+  `${speedConverter(55.7, meterPerSecUnit)} ${kilometerPerHourUnit}`
+);
 
 // Testing function to double value
-console.log(double(100.06))
+const value = 100.06
+const times = 2
+console.log("multiply", value, "by", 2, "is equal to", multiplyBy(value, times));
 
 // Testing function to check if number is prime
-console.log(primeCheck(101))
+
+const valueToCheck = 101
+console.log(`Is ${valueToCheck} prime?`, primeCheck(valueToCheck))
